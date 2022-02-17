89c41ac36257be3da2f0f884934a67d2ee42568b
Yushin <yushin@hupapp.com>
2022-02-17 21:46:23 +1300

    Commit

--- Hello/ViewController.swift
+++ Hello/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var lblHello: UILabel!
+    @IBOutlet var txtName: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func btnSend(_ sender: UIButton) {
+        lblHello.text = "Hello, " + txtName.text!
+        myLogPrint("=== didFinishLaunchingWithOptions ===")
+    }
+    
+    public func myLogPrint(_ object: Any, _ file: String = #file, _ line: Int = #line, _ funcname: String = #function) {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "HH:mm:ss:SSS"
+        print("Log!! : \(dateFormatter.string(from: Date())) file: \(file) line: \(line) func: \(funcname)")
+        print(object)
+    }
+    
 }
 
